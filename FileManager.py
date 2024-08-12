6a1278ddbe0409003916b1b3483c053ff7bfb34a
maistyev <jeka.maystrenko99@gmail.com>
2024-08-12 23:28:03 +0300

    created FileManager class

---
+++ FileManager.py
@@ -0,0 +1,10 @@
+class FileManager:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def write(self, data):
+        pass
+
+    def read(self):
+        records = None
+        return records
