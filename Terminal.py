b93579b2cfae1965bcd5f5d257e32465410fe314
Yuliia Kozlova <juljuliakv@gmail.com>
2024-08-13 08:37:24 +0200

    class announcement

--- Terminal.py
+++ Terminal.py
@@ -1,4 +1,61 @@
 class Terminal:
     def __init__(self):
+        self.contacts = {}  
+        self.notes = {}     
+    
+    def add_contact(self, name, phone, email):
+       
+        pass
+    
+    def add_note(self, title, content):
+        
+        pass
+    
+    def search_contacts(self, name):
+        
+        pass
+    
+    def search_notes(self, title):
+        
+        pass
+    
+    def delete_contact(self, name):
+        
+        pass
+    
+    def delete_note(self, title):
+        
+        pass
+    
+    def edit_contact(self, name, phone=None, email=None):
+        
+        pass
+    
+    def edit_note(self, title, content):
+        
+        pass
+    
+    def list_birthdays(self):
+        
+        pass
+    
+    def validate_phone(self, phone):
+        
+        pass
+    
+    def validate_email(self, email):
+        
+        pass
+    
+    def tag_note(self, title, tag):
+        
+        pass
+    
+    def search_by_tag(self, tag):
+        
+        pass
+    
+    def suggest_command(self):
+        
         pass
     
