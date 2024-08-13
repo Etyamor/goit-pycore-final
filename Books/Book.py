772208e171d6369c75099cc60f0d2b36083a6cc3
maistyev <jeka.maystrenko99@gmail.com>
2024-08-13 19:42:05 +0300

    Added sorting by key with reverse option

--- Books/Book.py
+++ Books/Book.py
@@ -7,3 +7,12 @@ class Book(UserList):
     
     def find_entity(self, text: str) -> List:
         pass
+
+    def sort(self, key: str, reverse = False) -> List:
+        ''' Sorts the data by the key provided '''
+        for record in self.data:
+            if key not in record.__dict__:
+                raise ValueError(f"Key {key} does not exist in {record.__class__.__name__}")
+            
+        sorted_data = sorted(self.data, key = lambda record: getattr(record, key), reverse = reverse)
+        return sorted_data
