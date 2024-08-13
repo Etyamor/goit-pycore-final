7c015bf9d2260bb96d79b1bde42569feb4106c4a
Roma Yanush <vrong617@gmail.com>
2024-08-13 10:20:55 +0300

    Add validate for fields

--- Fields/Birthday.py
+++ Fields/Birthday.py
@@ -1,6 +1,14 @@
 from Fields.Field import Field
+from datetime import datetime
 
 
 class Birthday(Field):
     def __init__(self, value):
-        super().__init__(value)
+        try:
+            birthday_date = datetime.strptime(value, "%d.%m.%Y")
+            super().__init__(birthday_date)
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+        
+    def __str__(self):
+        return self.value.strftime("%d.%m.%Y")
--- Fields/Email.py
+++ Fields/Email.py
@@ -1,6 +1,20 @@
 from Fields.Field import Field
+import re
 
 
 class Email(Field):
     def __init__(self, value):
         super().__init__(value)
+
+    @property
+    def value(self):
+        return self.value
+    
+    @value.setter
+    def value(self, value):
+        email_regex = re.compile(
+            r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        )
+        if not email_regex.match(value):
+            raise ValueError("Invalid email address")
+        self._value = value
