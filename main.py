e138ab7c7bb52fd9a7d3ab4942c9b234392b4598
Maksym Rutkovskyi <maksym.rutkovskyi@Maksyms-Laptop.local>
2024-08-14 04:10:55 +0300

    fixed doubled main function

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
-
-
-from Terminal import Terminal 
+from Terminal import Terminal
 
 if __name__ == "__main__":
     terminal = Terminal()
